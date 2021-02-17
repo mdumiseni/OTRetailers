b8a68bf05d84f68f3d99f2554728f28caf0b60a7
mdumiseni <mdumiseni.nyandu@gmail.com>
2021-02-17 13:50:13 +0200

    Added frontend, backend , Test project and Database to project

---
+++ OTRetailSystem/OTRetailerWebApi/Controller/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace OTRetailerWebApi.Controller
+{
+    public class HomeController : ApiController
+    {
+        [HttpGet]
+        [Route("{controller}/Home")]
+        public IHttpActionResult Home()
+        {
+            return Ok("Welcome to OTRetailer web api....");
+        }
+    }
+}
---
+++ OTRetailSystem/OTRetailerWebApi/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+
+namespace OTRetailerWebApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseUrl = "http://localhost:8080";
+            var config = new HttpSelfHostConfiguration(baseUrl);
+
+            config.Routes.MapHttpRoute(
+               name: "API",
+               routeTemplate: "{controller}/{action}/{id}",
+                defaults: new
+                {
+                    id = RouteParameter.Optional
+                });
+
+
+            using (HttpSelfHostServer server = new HttpSelfHostServer(config))
+            {
+                server.OpenAsync().Wait();
+                Console.WriteLine($"Server start at {baseUrl}");
+                Console.WriteLine("Press any key to quit...");
+                Console.ReadLine(); 
+            }
+
+        }
+    }
+}
